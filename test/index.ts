34b53b5b8d9b0dbb618e57fa0ff440f5762552b0
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2018-02-25 22:32:35 +0000

    - Updated file typescript for test.

--- test/index.ts
+++ test/index.ts
@@ -1,22 +1,31 @@
 class Animal {
-    name: string;
-    constructor(theName: string) { this.name = theName; }
-    move(distanceInMeters: number = 0) {
+    private name: string;
+    constructor(theName: string) {
+        this.name = theName;
+    }
+
+    public move(distanceInMeters: number = 0) {
         console.log(`${this.name} moved ${distanceInMeters}m.`);
     }
 }
 
 class Snake extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 5) {
+    constructor(name: string) {
+        super(name);
+    }
+
+    public move(distanceInMeters = 5) {
         console.log("Slithering...");
         super.move(distanceInMeters);
     }
 }
 
 class Horse extends Animal {
-    constructor(name: string) { super(name); }
-    move(distanceInMeters = 45) {
+    constructor(name: string) {
+        super(name);
+    }
+
+    public move(distanceInMeters = 45) {
         console.log("Galloping...");
         super.move(distanceInMeters);
     }
